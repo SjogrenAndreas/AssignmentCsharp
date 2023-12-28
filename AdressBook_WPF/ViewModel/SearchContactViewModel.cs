930c56249830e8fa46bcf27471b0685aebababfa
Andreas <143494824+Adoroar@users.noreply.github.com>
2023-12-28 21:31:04 +0100

    updated

---
+++ AdressBook_WPF/ViewModel/SearchContactViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using AdressBook_WPF.Models;
+using AdressBook_WPF.Services;
+using System;
+using System.Linq;
+using System.Windows;
+
+public partial class SearchContactViewModel : ObservableObject
+{
+    private readonly AddressBookService _addressBookService;
+    private readonly Action<Contact> _navigateToContactDetails;
+    private readonly Action _navigateToMain;
+
+    public SearchContactViewModel(AddressBookService addressBookService,
+                                  Action<Contact> navigateToContactDetails,
+                                  Action navigateToMain)
+    {
+        _addressBookService = addressBookService;
+        _navigateToContactDetails = navigateToContactDetails;
+        _navigateToMain = navigateToMain;
+    }
+
+    [ObservableProperty]
+    private string _emailToSearch;
+
+    [ICommand]
+    private void Search()
+    {
+        var foundContact = _addressBookService.GetAllContacts()
+                            .FirstOrDefault(c => c.Email.Equals(EmailToSearch, StringComparison.OrdinalIgnoreCase));
+        if (foundContact != null)
+        {
+            _navigateToContactDetails?.Invoke(foundContact);
+        }
+        else
+        {
+            MessageBox.Show("Could not find a contact with that email address", "Search Result", MessageBoxButton.OK);
+        }
+    }
+
+    [ICommand]
+    private void Cancel()
+    {
+        _navigateToMain?.Invoke();
+    }
+}
---
+++ AdressBook_WPF/Views/SearchContactView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AdressBook_WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for SearchContactView.xaml
+    /// </summary>
+    public partial class SearchContactView : Window
+    {
+        public SearchContactView()
+        {
+            InitializeComponent();
+        }
+
+        private void EmailTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Enter Email to Search")
+            {
+                textBox.Text = "";
+                textBox.FontStyle = FontStyles.Normal;
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void EmailTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Enter Email to Search";
+                textBox.FontStyle = FontStyles.Italic;
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
+    }
+}

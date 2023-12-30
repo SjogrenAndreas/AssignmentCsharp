1a6405c36d344038d0950a769d2b2e550988cc86
Andreas <143494824+Adoroar@users.noreply.github.com>
2023-12-30 12:30:17 +0100

    updated

--- AdressBook_WPF/Models/Contact.cs
+++ AdressBook_WPF/Models/Contact.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AdressBook_WPF.Models;               
-                                                  
-public class Contact 
+namespace AdressBook_WPF.Models
 {
+    public class Contact
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string StreetName { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
 
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string PhoneNumber { get; set; } = null!;
-    public string StreetName { get; set; } = null!;
-    public string PostalCode { get; set; } = null!;
-    public string City { get; set; } = null!;
+        // Fullständigt namn, består av förnamn och efternamn
+        public string FullName => $"{FirstName} {LastName}";
+    }
 }

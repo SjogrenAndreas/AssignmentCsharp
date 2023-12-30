fd0aaa831fcf0fc117ee558f69f11a18445f0ffa
Andreas <143494824+Adoroar@users.noreply.github.com>
2023-12-30 19:32:29 +0100

    updated finished

---
+++ AdressBook_WPF.tests/AdressBook_WPF.tests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using AdressBook_WPF.Models;
+using AdressBook_WPF.Services;
+using System;
+using System.Linq;
+
+public class AddressBookServiceTests
+{
+    [Fact]
+    public void AddContact_ShouldAddContactToList()
+    {
+        // Arrange
+        var service = new AddressBookService(); 
+        var testContact = new Contact
+        {
+            FirstName = "Test",
+            LastName = "User",
+            Email = "test@example.com"
+            
+        };
+
+        // Act
+        service.AddContact(testContact);
+
+        // Assert
+        var result = service.GetAllContacts().Any(c => c.Email == "test@example.com");
+        Assert.True(result);
+    }
+
+    
+}
